f64d43137d81b26282def2d9532843b148e6bb98
Vaibhav Mishra <vaibhav.mishra245@gmail.com>
2024-10-08 08:06:13 +0530

    GET,POST,Delete....using Postman

--- src/app.js
+++ src/app.js
@@ -1,19 +1,39 @@
 // console.log("This is first line of my first node js backend project")
 const express = require("express");
-
+// order of the routes matter a lots
+ //If first route matches with the routes decleared below , it will always go into 1st route. 
 const app = express();
-app.use("/home",(req,res)=>{
-    res.send("Hello from the home")
+app.use("/user",(req,res)=>{
+ res.send("ABRACADABRA!!!!!!")
 })
-app.use("/login",(req,res)=>{
-    res.send("Hello from the login")
+app.get("/user",(req,res)=>{
+    res.send("this is to GET the user")
 })
-app.use("/about",(req,res)=>{
-    res.send("Hello from the about")
+app.post("/user",(req,res)=>{
+    res.send("this is to POST the user")
 })
-app.use("/type",(req,res)=>{
-    res.send("Hello from the type")
+app.delete("/user",(req,res)=>{
+    res.send("this is to delete the user")
 })
+// app.use("/login",(req,res)=>{ 
+//     res.send("Hello from the login")
+// })
+// app.use("/login222",(req,res)=>{ 
+//     res.send("Hello from the login222")
+// })
+// app.use("/login/4",(req,res)=>{
+//     res.send("Hello from the login 4")
+// })
+// app.use("/about",(req,res)=>{
+//     res.send("Hello from the about")
+// })
+// app.use("/type",(req,res)=>{
+//     res.send("Hello from the type")
+// })
+
+// app.use("/",(req,res)=>{
+//     res.send("Hello from the / route")
+// })
 app.listen(3001,()=>{
     console.log("Server is successfully listening on port::3001")
 })
